03edf51a6f8bbacf428476c94ebe0169566606c3
01lasiaf <01lasiaf@users.noreply.github.com>
2023-12-09 02:21:14 +0700

    uploaded again

---
+++ uasweb2/application/views/kendaraan.php
@@ -0,0 +1,52 @@
+<!-- container fluid with carousel indicators -->
+<div class="container-fluid">
+  <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
+    <ol class="carousel-indicators">
+      <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
+      <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
+      <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
+      <li data-target="#carouselExampleIndicators" data-slide-to="3"></li>
+    </ol>
+    <div class="carousel-inner">
+      <div class="carousel-item active">
+        <img class="d-block w-100" src="<?php echo base_url('assets/img/fasilkom.jpg') ?>" alt="First slide">
+      </div>
+      <div class="carousel-item">
+        <img class="d-block w-100" src="<?php echo base_url('assets/img/slide4.jpg') ?>" alt="Second slide">
+      </div>
+      <div class="carousel-item">
+        <img class="d-block w-100" src="<?php echo base_url('assets/img/Peringkat SIR.webp') ?>" alt="Third slide">
+      </div>
+      <div class="carousel-item">
+        <img class="d-block w-100" src="<?php echo base_url('assets/img/Banner webometrics.webp') ?>" alt="Fourth slide">
+      </div>
+    </div>
+    <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
+      <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+      <span class="sr-only">Previous</span>
+    </a>
+    <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
+      <span class="carousel-control-next-icon" aria-hidden="true"></span>
+      <span class="sr-only">Next</span>
+    </a>
+  </div>  
+</div>
+
+          <div class="row text-center mt-3">
+
+              <?php foreach ($kendaraan as $brg) : ?>
+                <div class="card ml-4 mb-3" style="width: 16rem;">
+          <img src="<?php echo base_url(). "/uploads/" .$brg->gambar ?>" class="card-img-top" alt="...">
+          <div class="card-body">
+            <h5 class="card-title mb-1"><?php echo $brg->nama_brg ?></h5>
+            <small><?php echo $brg->keterangan ?></small><br>
+            <span class="badge badge-success mb-3">Rp. <?php echo number_format($brg->harga, 0,',','.')  ?></span>
+            <?php echo anchor('dashboard/tambah_ke_keranjang/'.$brg->id_brg,'<div class="btn btn-sm btn-primary">Tambah ke Keranjang</div>') ?>
+            <?php echo anchor('dashboard/detail/'.$brg->id_brg,'<div class="btn btn-sm btn-primary">Detail</div>') ?>
+  </div>
+</div>
+
+      <?php endforeach; ?> 
+    
+  </div>
+</div>
---
+++ uasweb2/assets/js/sb-admin-2.js
@@ -0,0 +1,68 @@
+(function($) {
+  "use strict"; // Start of use strict
+
+  // Toggle the side navigation
+  $("#sidebarToggle, #sidebarToggleTop").on('click', function(e) {
+    $("body").toggleClass("sidebar-toggled");
+    $(".sidebar").toggleClass("toggled");
+    if ($(".sidebar").hasClass("toggled")) {
+      $('.sidebar .collapse').collapse('hide');
+    };
+  });
+
+  // Close any open menu accordions when window is resized below 768px
+  $(window).resize(function() {
+    if ($(window).width() < 768) {
+      $('.sidebar .collapse').collapse('hide');
+    };
+  });
+
+  // Prevent the content wrapper from scrolling when the fixed side navigation hovered over
+  $('body.fixed-nav .sidebar').on('mousewheel DOMMouseScroll wheel', function(e) {
+    if ($(window).width() > 768) {
+      var e0 = e.originalEvent,
+        delta = e0.wheelDelta || -e0.detail;
+      this.scrollTop += (delta < 0 ? 1 : -1) * 30;
+      e.preventDefault();
+    }
+  });
+
+  // Scroll to top button appear
+  $(document).on('scroll', function() {
+    var scrollDistance = $(this).scrollTop();
+    if (scrollDistance > 100) {
+      $('.scroll-to-top').fadeIn();
+    } else {
+      $('.scroll-to-top').fadeOut();
+    }
+  });
+
+  // Smooth scrolling using jQuery easing
+  $(document).on('click', 'a.scroll-to-top', function(e) {
+    var $anchor = $(this);
+    $('html, body').stop().animate({
+      scrollTop: ($($anchor.attr('href')).offset().top)
+    }, 1000, 'easeInOutExpo');
+    e.preventDefault();
+  });
+
+})(jQuery); // End of use strict
+
+// #1 Function to scroll to the top of the page
+function scrollToTop() {
+  // For smooth scrolling, you can use the following code
+  document.body.scrollTop = 0; // For Safari
+  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE, and Opera
+}
+
+// Show/hide the Scroll to Top button based on scroll position
+window.onscroll = function() {
+  var scrollToTopBtn = document.getElementById("scrollToTopBtn");
+
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+      scrollToTopBtn.style.display = "block";
+  } else {
+      scrollToTopBtn.style.display = "none";
+  }
+};
+
